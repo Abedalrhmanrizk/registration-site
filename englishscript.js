5c28e8c4007a318f9e30162431634f1190290139
Tareq abu kharmh <tariq.abukharmh@gmail.com>
2023-05-17 14:51:15 +0300

    changes

---
+++ englishscript.js
@@ -0,0 +1,10 @@
+// ENGLISH TEST ELEMENTS
+import { quizData } from "./quiz.js";
+
+const questionEl = document.getElementById("question");
+const answerInput = document.querySelectorAll(".answer");
+const answerTextA = document.getElementById("a_text");
+const answerTextB = document.getElementById("b_text");
+const answerTextC = document.getElementById("c_text");
+const answerTextD = document.getElementById("d_text");
+const nextBtn = document.querySelector("next");
--- script.js
+++ script.js
@@ -14,13 +14,13 @@ const passwordInputLog = document.getElementById("password-log");
 const submitBtnLog = document.getElementById("submit-log");
 
 // ENGLISH TEST ELEMENTS
-const questionEl = document.getElementById("question");
-const answerInput = document.querySelectorAll(".answer");
-const answerTextA = document.getElementById("a_text");
-const answerTextB = document.getElementById("b_text");
-const answerTextC = document.getElementById("c_text");
-const answerTextD = document.getElementById("d_text");
-const nextBtn = document.querySelector("next");
+// const questionEl = document.getElementById("question");
+// const answerInput = document.querySelectorAll(".answer");
+// const answerTextA = document.getElementById("a_text");
+// const answerTextB = document.getElementById("b_text");
+// const answerTextC = document.getElementById("c_text");
+// const answerTextD = document.getElementById("d_text");
+// const nextBtn = document.querySelector("next");
 
 // STATUS PAGE
 const infoBtn = document.querySelector(".btn-apply-info");
