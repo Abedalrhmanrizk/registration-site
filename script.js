c8d2539f68cb60392daa60a1819e1ff756daa541
unknown <abedalrhmanrizk@gmail.comm>
2023-05-17 11:51:06 +0300

    add js file

---
+++ script.js
@@ -0,0 +1,29 @@
+// NAVBAR ELEMENT
+const loginBtn = document.querySelector(".login-btn");
+
+// REGISTRATION ELEMENTS
+const nameInput = document.getElementById("name");
+const emailInputRegi = document.getElementById("email");
+const passwordInputRegi = document.getElementById("password");
+const rePassword = document.getElementById("re-password");
+const submitBtnRegi = document.getElementById("submit");
+
+// LOGIN ELEMENTS
+const emailInputLog = document.getElementById("email-log");
+const passwordInputLog = document.getElementById("password-log");
+const submitBtnLog = document.getElementById("submit-log");
+
+// ENGLISH TEST ELEMENTS
+const questionEl = document.getElementById("question");
+const answerInput = document.querySelectorAll(".answer");
+const answerTextA = document.getElementById("a_text");
+const answerTextB = document.getElementById("b_text");
+const answerTextC = document.getElementById("c_text");
+const answerTextD = document.getElementById("d_text");
+const nextBtn = document.querySelector("next");
+
+// STATUS PAGE
+const infoBtn = document.querySelector(".btn-apply-info");
+const englishBtn = document.querySelector(".btn-apply-english");
+const technicalBtn = document.querySelector(".btn-apply-technical");
+const submitPage = document.querySelector(".submit-page");
