70fd09394c5ed7cd50e6360fec12c6ab360944aa
abedalrhmanrizk <abedalrhman.rizk.com>
2023-05-20 21:04:18 +0300

    add styles

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// NAVBAR ELEMENT
-// const loginBtn = document.querySelector(".login-btn");
-
-// REGISTRATION ELEMENTS
-// const nameInput = document.getElementById("name");
-// const emailInputRegi = document.getElementById("email");
-// const passwordInputRegi = document.getElementById("password");
-// const rePassword = document.getElementById("re-password");
-// const submitBtnRegi = document.getElementById("submit");
-
-// LOGIN ELEMENTS
-// const emailInputLog = document.getElementById("email-log");
-// const passwordInputLog = document.getElementById("password-log");
-// const submitBtnLog = document.getElementById("submit-log");
-
-// ENGLISH TEST ELEMENTS
-// const questionEl = document.getElementById("question");
-// const answerInput = document.querySelectorAll(".answer");
-// const answerTextA = document.getElementById("a_text");
-// const answerTextB = document.getElementById("b_text");
-// const answerTextC = document.getElementById("c_text");
-// const answerTextD = document.getElementById("d_text");
-// const nextBtn = document.querySelector("next");
-
 // INDEX OF ARRAY USER
 let index = sessionStorage.getItem("index")
 let Data = JSON.parse(localStorage.getItem("Data"))
@@ -70,10 +46,17 @@ logout.addEventListener("click", function () {
 
 let table = document.querySelector(".table")
 let hero = document.querySelector(".hero")
-let x = document.querySelector(".x")
+let submitAll = document.querySelector(".submit-all")
 let showTable = document.querySelector(".showTable")
 
+// if (
+//   Data[index].taskE !== "" &&
+//   Data[index].profile !== "" &&
+//   Data[index].taskT !== "" &&
+//   Data[index].answerAll == ""
+// ) {
 
+// }
 
 if (
   Data[index].taskE !== "" &&
@@ -81,7 +64,28 @@ if (
   Data[index].taskT !== "" &&
   Data[index].answerAll == ""
 ) {
-  x.classList.remove("hidden")
+  submitAll.classList.remove("hidden")
+  submitAll.addEventListener("click", () => {
+    table.classList.remove("hidden")
+    hero.classList.add("hidden")
+    Data[index].answerAll = "Done"
+    submitAll.remove()
+    showTable.innerHTML = `
+    <tr>
+      <th>${Data[index].nameInput}</th>
+      <td>${Data[index].emailInputRegi}</td>
+      <td>${Data[index].education}</td>
+      <td>${Data[index].personalData}</td>
+      <td>${Data[index].taskE} / 15</td>
+      <td>${Data[index].taskT} / 10</td>
+    </tr>
+  `
+  })
+} else {
+  table.classList.remove("hidden")
+  hero.classList.add("hidden")
+  submitAll.remove()
+  submitAll.classList.remove("hidden")
   Data[index].answerAll = "Done"
   showTable.innerHTML = `
     <tr>
@@ -92,17 +96,5 @@ if (
       <td>${Data[index].taskE} / 15</td>
       <td>${Data[index].taskT} / 10</td>
     </tr>
-  `;  
-}
-
-if (Data[index].answerAll == "") {
-  x.addEventListener("click", () => {
-    table.classList.remove("hidden")
-    hero.classList.add("hidden")
-    x.remove()
-  })
-} else {
-  table.classList.remove("hidden")
-  hero.classList.add("hidden")
-  x.remove()
+  `
 }
