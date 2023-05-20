60094663ab301f30dbc0e9f39b184de3d9741fea
jehad-mansra <jehad.mansra@gmail.com>
2023-05-20 11:10:34 +0300

    jehad

--- js/singup.js
+++ js/singup.js
@@ -1,5 +1,5 @@
-// username => mkyong-100@yahoo.com 
-// password => gr3at@3wdsG 
+// username => mkyong-100@yahoo.com
+// password => gr3at@3wdsG
 
 // REGISTRATION ELEMENTS
 const nameInput = document.getElementById("name");
@@ -8,10 +8,10 @@ const passwordInputRegi = document.getElementById("password");
 const rePassword = document.getElementById("re-password");
 const submitBtnRegi = document.getElementById("submit");
 
-
 // REGX CHECK
 let checkEmail = "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}";
-let checkPass = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$";
+let checkPass =
+  "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$";
 
 // ERROR SPAN
 let spanName = document.querySelector(".error-name");
@@ -19,7 +19,7 @@ let spanEmail = document.querySelector(".error-email");
 let spanPass = document.querySelector(".error-password");
 let spanPassrep = document.querySelector(".error-passwRep");
 
-// ERROR TAG 
+// ERROR TAG
 let error = document.createElement("i");
 error.setAttribute("class", "fa-solid fa-exclamation");
 // WARNING TAG
@@ -27,108 +27,113 @@ let warning = document.createElement("i");
 warning.setAttribute("class", "warning fa-solid fa-triangle-exclamation");
 
 submitBtnRegi.addEventListener("click", (e) => {
-    e.preventDefault();
-    // DELETE TEXT ERROR
-    spanName.textContent = "";
-    spanEmail.textContent = "";
-    spanPass.textContent = "";
-    spanPassrep.textContent = "";
+  e.preventDefault();
+  // DELETE TEXT ERROR
+  spanName.textContent = "";
+  spanEmail.textContent = "";
+  spanPass.textContent = "";
+  spanPassrep.textContent = "";
 
-    if (nameInput.value !== "" && emailInputRegi.value !== "" && passwordInputRegi.value !== "") {
-        if (emailInputRegi.value.match(checkEmail)) {
-            if (passwordInputRegi.value.match(checkPass)) {
-                if (passwordInputRegi.value === rePassword.value) {
-                    let obj = {
-                        nameInput: nameInput.value,
-                        emailInputRegi: emailInputRegi.value,
-                        passwordInputRegi: passwordInputRegi.value,
-                        data: "",
-                        taskT: "",
-                        taskE: "",
-                    }
+  if (
+    nameInput.value !== "" &&
+    emailInputRegi.value !== "" &&
+    passwordInputRegi.value !== ""
+  ) {
+    if (emailInputRegi.value.match(checkEmail)) {
+      if (passwordInputRegi.value.match(checkPass)) {
+        if (passwordInputRegi.value === rePassword.value) {
+          let obj = {
+            nameInput: nameInput.value,
+            emailInputRegi: emailInputRegi.value,
+            passwordInputRegi: passwordInputRegi.value,
+            data: "",
+            taskT: "",
+            taskE: "",
+          };
 
-                    // CHECK LOCALSTORAGE DATA 
-                    if (localStorage.getItem('Data') == null) {
-                        localStorage.setItem('Data', JSON.stringify([]));
-                    }
+          // CHECK LOCALSTORAGE DATA
+          if (localStorage.getItem("Data") == null) {
+            localStorage.setItem("Data", JSON.stringify([]));
+          }
 
-                    // SET LOCALSTORAGE DATA 
-                    let data = JSON.parse(localStorage.getItem('Data'));
-                    if (data.length == 0) {
-                        data.push(obj);
-                        localStorage.setItem('Data', JSON.stringify(data));
-                    }
+          // SET LOCALSTORAGE DATA
+          let data = JSON.parse(localStorage.getItem("Data"));
+          
 
-                    // CHECK VALID EMAIL 
-                    for (const ele of data) {
-                        if (ele.emailInputRegi == emailInputRegi.value) {
-                            return;
-                        }
-                    }
-                    // INSERT DATA
-                    data.push(obj);
-                    localStorage.setItem('Data', JSON.stringify(data));
-
-                } else {// VALIDATION CHECK PASSWORD === PASSWORDREP
-                    let text = document.createTextNode("Passwords do not match");
-                    error.appendChild(text);
-                    spanPass.appendChild(error);
-                    spanPass.appendChild(text);
-                }
-            } else {// VALIDATION CHECK REGX PASSWORD
-                let text1 = document.createTextNode("It must contain an uppercase and lowercase letter and a number");
-                warning.appendChild(text1);
-                spanPass.appendChild(warning);
-                spanPass.appendChild(text1);
+          // CHECK VALID EMAIL
+          for (const ele of data) {
+            if (ele.emailInputRegi == emailInputRegi.value) {
+                return;
             }
-        } else {// VALIDATION CHECK REGX PASSWORD
-            let text = document.createTextNode("Email Error .. ex : mkyong-100@yahoo.com");
-            error.appendChild(text);
-            spanEmail.appendChild(error);
-            spanEmail.appendChild(text);
-
-            let text1 = document.createTextNode("It must contain an uppercase and lowercase letter and a number");
-            warning.appendChild(text1);
-            spanPass.appendChild(warning);
-            spanPass.appendChild(text1);
+          }
+          // INSERT DATA
+          data.push(obj);
+          localStorage.setItem("Data", JSON.stringify(data));
+        } else {
+          // VALIDATION CHECK PASSWORD === PASSWORDREP
+          let text = document.createTextNode("Passwords do not match");
+          error.appendChild(text);
+          spanPass.appendChild(error);
+          spanPass.appendChild(text);
         }
-    } else { // HIS FIELD IS REQUIRED 
+      } else {
+        // VALIDATION CHECK REGX PASSWORD
+        let text1 = document.createTextNode(
+          "It must contain an uppercase and lowercase letter and a number"
+        );
+        warning.appendChild(text1);
+        spanPass.appendChild(warning);
+        spanPass.appendChild(text1);
+      }
+    } else {
+      // VALIDATION CHECK REGX PASSWORD
+      let text = document.createTextNode(
+        "Email Error .. ex : mkyong-100@yahoo.com"
+      );
+      error.appendChild(text);
+      spanEmail.appendChild(error);
+      spanEmail.appendChild(text);
 
-        if (nameInput.value === "") {
-            let error = document.createElement("i");
-            let text = document.createTextNode("This field is required");
-            error.setAttribute("class", "fa-solid fa-exclamation");
-            spanName.appendChild(error);
-            spanName.appendChild(text);
-        }
-
-        if (emailInputRegi.value === "") {
-            let error = document.createElement("i");
-            let text = document.createTextNode("This field is required");
-            error.setAttribute("class", "fa-solid fa-exclamation");
-            spanEmail.appendChild(error);
-            spanEmail.appendChild(text);
-        }
-
-        if (passwordInputRegi.value === "") {
-            let error = document.createElement("i");
-            let text = document.createTextNode("This field is required");
-            error.setAttribute("class", "fa-solid fa-exclamation");
-            spanPass.appendChild(error);
-            spanPass.appendChild(text);
-        }
-
-        if (rePassword.value === "") {
-            let error = document.createElement("i");
-            let text = document.createTextNode("This field is required");
-            error.setAttribute("class", "fa-solid fa-exclamation");
-            spanPassrep.appendChild(error);
-            spanPassrep.appendChild(text);
-        }
+      let text1 = document.createTextNode(
+        "It must contain an uppercase and lowercase letter and a number"
+      );
+      warning.appendChild(text1);
+      spanPass.appendChild(warning);
+      spanPass.appendChild(text1);
     }
+  } else {
+    // HIS FIELD IS REQUIRED
+
+    if (nameInput.value === "") {
+      let error = document.createElement("i");
+      let text = document.createTextNode("This field is required");
+      error.setAttribute("class", "fa-solid fa-exclamation");
+      spanName.appendChild(error);
+      spanName.appendChild(text);
+    }
+
+    if (emailInputRegi.value === "") {
+      let error = document.createElement("i");
+      let text = document.createTextNode("This field is required");
+      error.setAttribute("class", "fa-solid fa-exclamation");
+      spanEmail.appendChild(error);
+      spanEmail.appendChild(text);
+    }
+
+    if (passwordInputRegi.value === "") {
+      let error = document.createElement("i");
+      let text = document.createTextNode("This field is required");
+      error.setAttribute("class", "fa-solid fa-exclamation");
+      spanPass.appendChild(error);
+      spanPass.appendChild(text);
+    }
+
+    if (rePassword.value === "") {
+      let error = document.createElement("i");
+      let text = document.createTextNode("This field is required");
+      error.setAttribute("class", "fa-solid fa-exclamation");
+      spanPassrep.appendChild(error);
+      spanPassrep.appendChild(text);
+    }
+  }
 });
-
-
-
-
-
